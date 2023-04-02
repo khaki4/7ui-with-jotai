6d2961b4ecc079ae7757a52e74ccb27e161af056
jiwonjung <4dakto@gmail.com>
2023-04-02 16:13:38 +0900

    [REFACTORING] code split

--- src/tasks/TemperatureConverter/index.tsx
+++ src/tasks/TemperatureConverter/index.tsx
@@ -2,14 +2,30 @@ import { useAtom } from "jotai";
 import { celsiusAtom, fahrenheitAtom } from "./atoms";
 
 export function TemperatureConverter() {
-  const [ct, setCt] = useAtom(celsiusAtom);
-  const [ft, setFt] = useAtom(fahrenheitAtom);
   return (
     <div style={{ display: "flex", gap: "10px" }}>
+      <Celsius />
+      <Fahrenheit />
+    </div>
+  );
+}
+
+function Celsius() {
+  const [ct, setCt] = useAtom(celsiusAtom);
+  return (
+    <>
       <input type="text" value={ct} onChange={(e) => setCt(e.target.value)} />
       <span>Celsius = </span>
+    </>
+  );
+}
+
+function Fahrenheit() {
+  const [ft, setFt] = useAtom(fahrenheitAtom);
+  return (
+    <>
       <input type="text" value={ft} onChange={(e) => setFt(e.target.value)} />
       <span>Fahrenheit</span>
-    </div>
+    </>
   );
 }
